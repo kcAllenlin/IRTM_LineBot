f483a4eeaa6acc8b0adc9d3c14c3fd4baa586f0c
fangyucha <fangyu@g.ncu.edu.tw>
2023-11-20 05:28:36 +0800

    fix action

--- daily_notify.py
+++ daily_notify.py
@@ -116,6 +116,7 @@ def line_notify(token, msg):
     
     payload = {'message': msg}
     r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+    print(r.status_code)
     return r.status_code
 
 if __name__ == '__main__':
