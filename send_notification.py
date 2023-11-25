062bd834a5a5ffd635d9598c8ba009ee2b22a914
kcAllenlin <91825222+kcAllenlin@users.noreply.github.com>
2023-11-25 09:23:13 +0800

    Update send_notification.py

--- send_notification.py
+++ send_notification.py
@@ -3,7 +3,7 @@ def send_notification(id):
     f = open(file, "r")
     title = f.readline()
     for row in f.readlines():
-        item = row.strip().split()
+        item = row.strip().split(",")
         if item[0] == user_company[id]:  #變數應該會隨存放位置不同而需要修改
             if item[3] == "n":
                 print(f"您的公司：{user_company[id]}，今天有一篇新聞的情緒為負")
