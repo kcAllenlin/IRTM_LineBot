9bfe7b0513886b445e87a05b9702c83a3bebd92f
fangyucha <fangyu@g.ncu.edu.tw>
2023-12-11 19:53:16 +0800

    fix notificaton

--- notification.py
+++ notification.py
@@ -1,5 +1,6 @@
-from app import user_id_lst, user_company, stock_name, line_bot_api, send_alert_message
+from app import user_id_lst, user_company, stock_name, send_alert_message
 
+line_bot_api = LineBotApi('yMp/ki+UImkB+yK0i//GBsQHZx9rePTIk8iZkAQeoGlzoSKaGdMikyvqwEy0d3gGItJYL7CEOdnR7rUMFCDuNZfQyLlyD7jA3am7mtJi3/5z+wmIR7GZFhL3DO/VBBP/ba4EWxz78WF2fc661kbCPAdB04t89/1O/w1cDnyilFU=')
 print(user_id_lst)
 print(user_company)
 
