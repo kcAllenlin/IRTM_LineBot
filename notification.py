64e118ed4fb333525895d5c0d7c09c7c3b5f479a
fangyucha <fangyu@g.ncu.edu.tw>
2023-12-22 13:36:36 +0800

    test send alert function

--- notification.py
+++ notification.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
 from linebot import LineBotApi
+from linebot.models import TextSendMessage, 
 import pandas as pd 
 
 db_url = os.environ['DATABASE_URL']
@@ -36,22 +37,13 @@ def get_company_name_from_database(user_id):
 def send_alert_message():    
     try:
         user_ids = get_all_user_ids()
-        print(user_ids)
         for user in user_ids:
-            print(user)
             company_name = get_company_name_from_database(user)
-            print(company_name)
             if company_name != None:
                 for i in range(df.shape[0]):
                     row = df.iloc[i]
-                    print(row["name"])
                     if row["name"] == company_name:
-                        print('yes1')
-                        print(row["type"])
                         if row["type"] == "n":
-                            print('yes2')
-                            print(row["url"])
-                            print(row["summary"])
                             message = [TextSendMessage(f"您的公司：{company_name}，今天有一篇新聞的情緒為負"), TextSendMessage(f"網址：{row['url']}"), TextSendMessage(f"文章概要：{row['summary']}")]
                             line_bot_api.push_message(user, message)
     except:
