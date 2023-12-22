c74a61c5876b8a8dd67c0ace29d39b863ce6d6b2
fangyucha <fangyu@g.ncu.edu.tw>
2023-12-22 13:09:08 +0800

    test send alert function

--- notification.py
+++ notification.py
@@ -34,15 +34,18 @@ def send_alert_message():
     try:
         user_ids = get_all_user_ids()
         for user in user_ids:
+            print(user)
             company_name = get_company_name_from_database(user)
+            print(company_name)
             if company_name != None:
                 with open("./data/analyisis.csv", "r") as csvfile:
                     data = csv.DictReader(csvfile)
                     for row in data:
+                        print(row)
                         if row["name"] == company_name:
                             if row["type"] == "n":
                                 message = [TextSendMessage(f"您的公司：{company_name}，今天有一篇新聞的情緒為負"), TextSendMessage(f"網址：{row['url']}"), TextSendMessage(f"文章概要：{row['summary']}")]
                                 print(message)
                                 line_bot_api.push_message(user, message)
     except:
-      pass
+        pass
